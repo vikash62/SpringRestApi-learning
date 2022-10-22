4aa634432fef8423e247fa71efd694a24c597bf7
Vikash <vikashpathak041@gmail.com>
2022-10-22 11:58:22 +0530

    Request param and path variable getting id

--- src/main/java/com/bridgelabz/springrestapi/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/springrestapi/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.springrestapi.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,16 @@ public class EmployeeController {
 	 @GetMapping("/employees")
 	 public String getEmployees() {
 		 return " displaying list of employee";
+	 }
+	 
+	 @GetMapping("/employees/{id}")
+//	 public String getEmployee(@PathVariable("id")Long id) 
+	 public String getEmployee(@PathVariable Long id) 
+	 {
+		 return " displaying list of employee id"+id;
+	 }
+	 @DeleteMapping("/employes")
+	 public String deleteEmployee(@RequestParam("id")Long id) {
+		 return " delete list of employee for id"+id;
 	 }
 } 
